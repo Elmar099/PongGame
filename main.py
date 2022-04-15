00da5aeb70ab341db8505534a418b034bd7effc5
Elmar0991~ <elmar6999@gmail.com>
2022-04-15 14:43:04 -0700

    final

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
-import turtle
+# Simple Pong in Python 3 for Beginners
+# By @TokyoEdTech
 
-win = turtle.Screen()
-win.title("Pong game by Elmar")
-win.bgcolor("white")
-win.setup(width=800, height=600)
-win.tracer(0)
+import turtle
+import os
+
+wn = turtle.Screen()
+wn.title("Pong")
+wn.bgcolor("white")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Score
+score_a = 0
+score_b = 0
 
 # Paddle A
 paddle_a = turtle.Turtle()
-paddle_a.speed(0)
+paddle_a.speed(3)
 paddle_a.shape("square")
-paddle_a.color("black")
-paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.color("green")
+paddle_a.shapesize(stretch_wid=7, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
 
 # Paddle B
 paddle_b = turtle.Turtle()
-paddle_b.speed(0)
+paddle_b.speed(3)
 paddle_b.shape("square")
-paddle_b.color("black")
-paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.color("blue")
+paddle_b.shapesize(stretch_wid=7, stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
+
 # Ball
 ball = turtle.Turtle()
 ball.speed(0)
@@ -30,14 +39,30 @@ ball.shape("circle")
 ball.color("purple")
 ball.penup()
 ball.goto(0, 0)
-ball.dx = 2
-ball.dy = 2
+ball.dx = 0.3
+ball.dy = 0.3
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("black")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("arial", 18, "normal"))
 
 
-# Function
+# Functions
 def paddle_a_up():
     y = paddle_a.ycor()
     y += 20
+    paddle_a.sety(y)
+
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
     paddle_a.sety(y)
 
 
@@ -53,37 +78,54 @@ def paddle_b_down():
     paddle_b.sety(y)
 
 
-def paddle_a_down():
-    y = paddle_a.ycor()
-    y -= 20
-    paddle_a.sety(y)
+# Keyboard bindings
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
 
-
-# Keyboard Binding
-win.listen()
-win.onkeypress(paddle_a_up, "w")
-win.onkeypress(paddle_a_down, "s")
-win.onkeypress(paddle_b_up, "Up")
-win.onkeypress(paddle_b_down, "Down")
-# Main while loop
+# Main game loop
 while True:
-    win.update()
+    wn.update()
 
-    # Move ball
+    # Move the ball
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
 
-    # Border check
+    # Border checking
+
+    # Top and bottom
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
-    if ball.ycor() > -290:
+        os.system("afplay bounce.wav&")
+
+    elif ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
-    if ball.xcor() > 390:
-        ball.goto(0, 0)
-        ball.dx *= -1
-    if ball.xcor() > -390:
+        os.system("afplay bounce.wav&")
+
+    # Left and right
+    if ball.xcor() > 350:
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
         ball.goto(0, 0)
         ball.dx *= -1
 
+    elif ball.xcor() < -350:
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    # Paddle and ball collisions
+    if ball.xcor() < -340 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
+        ball.dx *= -1
+        os.system("afplay bounce.wav&")
+
+    elif ball.xcor() > 340 and ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50:
+        ball.dx *= -1
+        os.system("afplay bounce.wav&")
